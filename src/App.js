fdddb8e24b3f6ed3a5b53fb7ca03a6960ec3ac1a
JordanSipe <jsipe2017@gmail.com>
2021-05-12 17:12:29 -0400

    finished assignment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+
+const [nasaData, setnasaData] = useState([]);
+
+useEffect(() => {
+  axios.get('https://api.nasa.gov/#apod?api_key=DEMO_KEY')
+  .then(res => {
+    setnasaData(res.data);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+})
 
 function App() {
   return (
